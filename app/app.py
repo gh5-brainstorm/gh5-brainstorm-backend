54e048c3d50af81a7ed26ad8547b5da69c5669e7
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2024-07-12 17:25:26 +0700

    resolve conflict

--- app/app.py
+++ app/app.py
@@ -9,6 +9,39 @@ from pathlib import Path
 import logging
 import os
 
+import tensorflow as tf
+import tensorflow_hub as hub
+from PIL import Image, ImageOps
+import numpy as np
+import tf_keras as keras
+from scipy.spatial import distance
+import io
+
+model_url = "https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2"
+
+IMAGE_SHAPE = (244, 244)
+
+model = keras.Sequential([
+    hub.KerasLayer(model_url, input_shape=IMAGE_SHAPE+(3,))
+])
+
+def extract(file, is_from_user):
+  if (is_from_user == "yes"):
+    file = Image.open(io.BytesIO(file)).convert('L').resize(IMAGE_SHAPE)
+  else:
+     file = Image.open(file).convert('L').resize(IMAGE_SHAPE)
+
+#   file = np.array(file)    
+  file = np.stack((file,)*3, axis=-1)
+  file = np.array(file)/255.0
+
+  embedding = model.predict(file[np.newaxis, ...])
+
+  vgg16_feature_np = np.array(embedding)
+  flattended_feature = vgg16_feature_np.flatten()
+
+  return flattended_feature
+
 app = FastAPI()
 
 logging.basicConfig(level=logging.INFO)
@@ -35,8 +68,22 @@ def write_database(data):
 
 @app.post("/upload")
 async def create_upload_file(file: UploadFile = File(...)):
+    logging.info('Start comparing image uploaded by user')
+    try:
+        cat1 = extract('images/2560px-A-Cat.jpg', "no")
+
+        request_image = file.file.read()
+        # image_file_pil = Image.open(io.BytesIO(request_image))
+        extract_img = extract(request_image, "yes")
+
+        result = distance.cdist([extract_img], [cat1], metric = 'cosine')[0]
+    except Exception as e:
+        logging.error(f'Error occurred: {e}')
+        raise HTTPException(status_code=500, detail="Failed to comparing image")
+
     logging.info('Starting file upload process')
     try:
+        # upload image content
         file_content = await file.read()
         file_path = Path(IMAGES_DIR) / file.filename
         with open(file_path, 'wb') as f:
@@ -51,11 +98,16 @@ async def create_upload_file(file: UploadFile = File(...)):
         image_entry = {
             "id": image_id,
             "url": f"{ngrok_url}/images/{file.filename}",
+<<<<<<< HEAD
+=======
+            "base64": base64_content  # Store base64 content in database for future use,
+            # "similiarity_score": result
+>>>>>>> cc2f734d912076154320adbe8aa473f2ca68a02d
         }
         data.append(image_entry)
         write_database(data)
 
-        return {"status": "ok", "id": image_id, "url": image_entry["url"]}
+        return {"status": "ok", "id": image_id, "url": image_entry["url"], 'similirarity_score': result.tolist()}
     except Exception as e:
         logging.error(f'Error occurred: {e}')
         raise HTTPException(status_code=500, detail="Failed to upload file")
